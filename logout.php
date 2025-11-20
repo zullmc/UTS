6b672801884552e03705f504017fe7c2555bdd6c
cynoo <149306069+zullmc@users.noreply.github.com>
2025-11-20 20:40:01 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,11 +1,15 @@
 <?php
 session_start();
-
-// Hapus semua session
-session_unset();
+// hapus semua session
+$_SESSION = [];
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
 session_destroy();
-
-// Kembali ke halaman login
-header("Location: login.php");
+header('Location: index.php');
 exit;
 ?>
